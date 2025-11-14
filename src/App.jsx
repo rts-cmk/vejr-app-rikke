cbcff5bf3e19c68c58c41f0c7d01052e905c924c
Rikke Gleerup <rikkeglee@gmail.com>
2025-11-14 08:45:03 +0100

    Indhold i vejr app

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>Vejr App</h1>
+
+        <section>
+
+        </section>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
